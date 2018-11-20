eba7f092061e645aa8ae08f1548418cc2a2cdd76
SofusHN <sofushn@hotmail.dk>
2018-11-20 12:48:19 +0100

    Created hamburger menu

---
+++ DIceOut201118/ViewModel/MenuViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using DIceOut201118.Annotations;
+using DIceOut201118.View;
+
+namespace DIceOut201118.ViewModel
+{
+    public class MenuViewModel
+    {
+        public ObservableCollection<NavigationViewItemBase> NavigationItems { get; set; }
+
+        public MenuViewModel()
+        {
+            NavigationItems = new ObservableCollection<NavigationViewItemBase>()
+            {
+                new NavigationViewItem()
+                {
+                    Content = "Main Page",
+                    Icon = new SymbolIcon(Symbol.Home),
+                    Tag = typeof(MainPage)
+                }
+            };
+
+        }
+    }
+}
