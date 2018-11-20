3ee489c8990ed1d1d9c469335337257ddb40dcfc
SofusHN <sofu0185@edu.easj.dk>
2018-11-20 13:53:11 +0100

    virker ikke

--- DIceOut201118/Model/Terning.cs
+++ DIceOut201118/Model/Terning.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using DIceOut201118.Annotations;
 
 namespace DIceOut201118.Model
 {
-    class Terning
+    class Terning : INotifyPropertyChanged
     {
         private int _faceValue;
         private Random _random;
@@ -14,7 +17,11 @@ namespace DIceOut201118.Model
         public int FaceValue
         {
             get { return _faceValue; }
-            set { _faceValue = value; }
+            set
+            {
+                _faceValue = value;
+                OnPropertyChanged();
+            }
         }
 
         public Terning()
@@ -31,8 +38,13 @@ namespace DIceOut201118.Model
 
         }
 
-        
 
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
