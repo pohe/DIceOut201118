799eac0d1b81089f050311baffdaabd74076e619
Anders Genderskov Binder <ande972k@edu.easj.dk>
2018-11-20 12:52:19 +0100

    Class Terning added

---
+++ DIceOut201118/Model/Terning.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIceOut201118.Model
+{
+    class Terning
+    {
+        private int _faceValue;
+
+        public int FaceValue
+        {
+            get { return _faceValue; }
+            set { _faceValue = value; }
+        }
+
+        public Terning()
+        {
+
+        }
+
+        public void Roll()
+        {
+            Random random = new Random();
+
+            _faceValue = random.Next(1, 6);
+
+        }
+
+        
+
+
+    }
+}
